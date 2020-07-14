42aaab42fe869fb32ad7418a41d926b4a60a69f0
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-07-13 19:08:49 -0700

    Adding Common and LinkedList project structure

---
+++ Common/src/com/mkv/datastructures/common/IOHandler.java
@@ -0,0 +1,9 @@
+package com.mkv.datastructures.common;
+
+public interface IOHandler<T> {
+
+	void print(T root);
+
+	void getUserInput();
+
+}
---
+++ LinkedList/src/com/mkv/datastructures/linkedlist/LinkedListNode.java
@@ -0,0 +1,11 @@
+package com.mkv.datastructures.linkedlist;
+
+public class LinkedListNode<T> {
+
+	T data;
+	LinkedListNode<T> next;
+
+	LinkedListNode(T data){
+		this.data = data;
+	}
+}
