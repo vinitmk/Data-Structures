27bc3f82e8e8e7be61c7285872d6a95667a67ba6
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-07-19 19:04:54 -0700

    Stack Using Array Basic structure

---
+++ Stack/src/com/mkv/datastructures/stack/StackUsingArray.java
@@ -0,0 +1,16 @@
+package com.mkv.datastructures.stack;
+
+import java.util.List;
+
+public class StackUsingArray<T> {
+
+	private List<T> stack;
+
+	public void push(){
+
+	}
+
+	public T pop(){
+		return null;
+	}
+}
