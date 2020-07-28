85cb77d1b25edc9ebe1b484987587788972062fc
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-07-27 19:23:46 -0700

    Stack Using Arraylist Push, Pop and Peek operations

--- Stack/src/com/mkv/datastructures/stack/StackUsingArray.java
+++ Stack/src/com/mkv/datastructures/stack/StackUsingArray.java
@@ -1,16 +1,50 @@
 package com.mkv.datastructures.stack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StackUsingArray<T> {
 
 	private List<T> stack;
 
-	public void push(){
-
+	private void push(T data){
+		if(null == stack) {
+			stack = new ArrayList<>();
+			stack.add(data);
+		} else {
+			stack.add(data);
+		}
 	}
 
-	public T pop(){
-		return null;
+	private T pop(){
+		if(null == stack)
+			return null;
+		else
+			return stack.remove(stack.size()-1);
+	}
+
+	private T peek(){
+		if(null == stack)
+			return null;
+		else
+			return stack.get(stack.size()-1);
+	}
+
+
+	public static void main(String[] args){
+		int d;
+		StackUsingArray<Integer> stack;
+		stack = new StackUsingArray<>();
+
+		stack.push(6);
+		stack.push(3);
+		stack.push(9);
+		d = stack.pop();
+		System.out.println("Popped "+ d);
+		d = stack.peek();
+		System.out.println("Peek "+ d);
+		stack.push(18);
+		d = stack.peek();
+		System.out.println("Peek "+ d);
 	}
 }
