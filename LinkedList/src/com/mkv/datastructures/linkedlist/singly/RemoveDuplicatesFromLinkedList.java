54d2e2039253af6391a8b14ded55fd9fec297dfd
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-08-27 11:42:41 -0700

    Remove Duplicates from LinkedList

---
+++ LinkedList/src/com/mkv/datastructures/linkedlist/singly/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,48 @@
+package com.mkv.datastructures.linkedlist.singly;
+
+public class RemoveDuplicatesFromLinkedList<T> {
+	private LinkedListMain<T> list;
+
+	private RemoveDuplicatesFromLinkedList() {
+		list = new LinkedListMain<>();
+	}
+
+	private void insertInLinkedList(T data) {
+		list.createDataStructure(data);
+	}
+
+	private void removeDuplicates() {
+		LinkedListNode<T> t = list.getHead();
+		LinkedListNode<T> tmp = t.next;
+		while(tmp != null) {
+			if(t.data == tmp.data) {
+				tmp = tmp.next;
+			}
+			else {
+				t.next = tmp;
+				t = t.next;
+			}
+		}
+
+		t.next = null;
+		list.print();
+	}
+
+	public static void main(String[] args) {
+		RemoveDuplicatesFromLinkedList<Integer> main = new RemoveDuplicatesFromLinkedList<>();
+
+		main.insertInLinkedList(11);
+		main.insertInLinkedList(11);
+		main.insertInLinkedList(11);
+		main.insertInLinkedList(23);
+		main.insertInLinkedList(23);
+		main.insertInLinkedList(45);
+		main.insertInLinkedList(45);
+		main.insertInLinkedList(45);
+		main.insertInLinkedList(60);
+
+		main.removeDuplicates();
+
+
+	}
+}
