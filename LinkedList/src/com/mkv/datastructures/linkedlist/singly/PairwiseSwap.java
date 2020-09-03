54e151df2f54e23f8fb79f4778c98e25762d9766
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-09-02 18:27:17 -0700

    Pairwise swapping LinkedList Nodes

---
+++ LinkedList/src/com/mkv/datastructures/linkedlist/singly/PairwiseSwap.java
@@ -0,0 +1,20 @@
+package com.mkv.datastructures.linkedlist.singly;
+
+public class PairwiseSwap<T> {
+
+	private void swapPairs(LinkedListNode<T> head) {
+
+	}
+
+	public static void main(String[] args) {
+		PairwiseSwap<Integer> main = new PairwiseSwap<>();
+
+		LinkedListMain<Integer> list = new LinkedListMain<>();
+		list.createDataStructure(1);
+		list.createDataStructure(2);
+		list.createDataStructure(3);
+		list.createDataStructure(4);
+		list.createDataStructure(5);
+		list.createDataStructure(6);
+	}
+}
