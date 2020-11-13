5ebfff92e8b399c13bc4ded236da6c309e382f36
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-11-12 23:40:58 -0800

    Binary Tree - LevelOrderTraversal

--- BinaryTree/src/com/mkv/datastructures/binarytree/BinaryTreeMain.java
+++ BinaryTree/src/com/mkv/datastructures/binarytree/BinaryTreeMain.java
@@ -8,18 +8,6 @@ public class BinaryTreeMain<T> {
 
 	private BinaryTreeNode<T> root;
 
-	public static void main(String[] args) {
-		Random num = new Random();
-		BinaryTreeMain<Integer> binaryTreeMain = new BinaryTreeMain<>();
-		System.out.println("Input data for Linked List ");
-
-		for (int i = 0; i < 10; i++) {
-			int n = num.nextInt(99);
-			binaryTreeMain.insert(n);
-		}
-		binaryTreeMain.print();
-	}
-
 	private void insert(T num) {
 		if (root == null) {
 
@@ -53,6 +41,9 @@ public class BinaryTreeMain<T> {
 		System.out.println();
 		printPostorder(root);
 		System.out.println();
+		printLevelOrder(root);
+		System.out.println();
+
 	}
 
 	private void printInorder(BinaryTreeNode<T> root) {
@@ -77,5 +68,35 @@ public class BinaryTreeMain<T> {
 			printInorder(root.right);
 			System.out.print(root.data + " ");
 		}
+	}
+
+	private void printLevelOrder(BinaryTreeNode<T> root) {
+		if(root != null) {
+			Queue<BinaryTreeNode<T>> queue = new ArrayDeque<>();
+			queue.add(root);
+			while(!queue.isEmpty()) {
+				BinaryTreeNode<T> node = queue.poll();
+				System.out.print(node.data + " ");
+				if(node.left != null)
+					queue.add(node.left);
+				if(node.right != null)
+					queue.add(node.right);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Random num = new Random();
+		BinaryTreeMain<Integer> binaryTreeMain = new BinaryTreeMain<>();
+		System.out.println("Input data for Linked List ");
+
+		for (int i = 0; i < 10; i++) {
+			int n = num.nextInt(99);
+			System.out.print(n + " ");
+
+			binaryTreeMain.insert(n);
+		}
+		System.out.println();
+		binaryTreeMain.print();
 	}
 }
