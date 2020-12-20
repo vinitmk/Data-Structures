c45ee0566adcc4d90011b51c1e0ad84b7102affc
vinitmk <vinit.kallampad@gmail.com>
2020-12-20 15:30:29 -0800

    All pair shortest path by Floyd Warshall

---
+++ Graphs/src/com/mkv/datastructures/APSPByFloydWarshallMain.java
@@ -0,0 +1,29 @@
+package com.mkv.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class APSPByFloydWarshallMain {
+
+	public static void main(String[] args) {
+
+		List<WeightedNode<String>> nodeList = new ArrayList<>();
+
+		nodeList.add(new WeightedNode<>("A"));
+		nodeList.add(new WeightedNode<>("B"));
+		nodeList.add(new WeightedNode<>("C"));
+		nodeList.add(new WeightedNode<>("D"));
+
+		FloydWarshall<String> allPairShortestPath = new FloydWarshall<>(nodeList);
+		allPairShortestPath.addWeightedEdge(0, 3, 1);// Add A-> D , weight 1
+		allPairShortestPath.addWeightedEdge(0, 1, 8);// Add A-> B , weight 8
+		allPairShortestPath.addWeightedEdge(1, 2, 1);// Add B-> C , weight 1
+		allPairShortestPath.addWeightedEdge(2, 0, 4);// Add C-> A , weight 4
+		allPairShortestPath.addWeightedEdge(3, 1, 2);// Add D-> B , weight 2
+		allPairShortestPath.addWeightedEdge(3, 2, 9);// Add D-> C , weight 9
+
+		System.out.println("Printing Floyd-Warshall from each source:");
+		allPairShortestPath.shortestPathByFloydWarshall();
+	}
+
+}
---
+++ Graphs/src/com/mkv/datastructures/FloydWarshall.java
@@ -0,0 +1,51 @@
+package com.mkv.datastructures;
+
+import java.util.List;
+
+class FloydWarshall<T> {
+
+	private List<WeightedNode<T>> nodeList;
+
+	FloydWarshall(List<WeightedNode<T>> nodeList) {
+		this.nodeList = nodeList;
+	}
+
+	void addWeightedEdge(int source, int destination, int weight) {
+		WeightedNode<T> first = nodeList.get(source);
+		WeightedNode<T> second = nodeList.get(destination);
+		first.getNeighbors().add(second);
+		first.getWeightMap().put(second, weight);
+	}
+
+	void shortestPathByFloydWarshall() {
+
+		int size = nodeList.size();
+		int[][] weights = new int[size][size];
+
+		for (int i = 0; i < size; i++) {
+			WeightedNode<T> first = nodeList.get(i);
+			for (int j = 0; j < size; j++) {
+				WeightedNode<T> second = nodeList.get(j);
+				// Dividing by 10 to avoid overflow while adding
+				if (i == j)
+					weights[i][j] = 0;
+				else
+					weights[i][j] = first.getWeightMap().getOrDefault(second, Integer.MAX_VALUE / 10);
+			}
+		}
+
+		for (int k = 0; k < size; k++)
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++)
+					if (weights[i][j] > weights[i][k] + weights[k][j])
+						weights[i][j] = weights[i][k] + weights[k][j];
+
+		for (int i = 0; i < size; i++) {
+			System.out.print("Printing distance list for node " + nodeList.get(i).getData() + ": ");
+			for (int j = 0; j < size; j++) {
+				System.out.print(weights[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
