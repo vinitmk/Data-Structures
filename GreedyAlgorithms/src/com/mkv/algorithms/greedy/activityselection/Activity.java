5513a95a64f9137043c77d2c4ab5e31666b5cd75
vinitmk <vinit.kallampad@gmail.com>
2021-01-03 21:37:02 -0800

    Activity Model Class

---
+++ GreedyAlgorithms/src/com/mkv/algorithms/greedy/activityselection/Activity.java
@@ -0,0 +1,48 @@
+package com.mkv.algorithms.greedy.activityselection;
+
+public class Activity implements Comparable<Activity> {
+
+	private String activityName;
+	private int startTime;
+	private int endTime;
+
+	String getActivityName() {
+		return activityName;
+	}
+
+	void setActivityName(String activityName) {
+		this.activityName = activityName;
+	}
+
+	int getStartTime() {
+		return startTime;
+	}
+
+	void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+
+	int getEndTime() {
+		return endTime;
+	}
+
+	void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity{" +
+				"activityName='" + activityName + '\'' +
+				", startTime=" + startTime +
+				", endTime=" + endTime +
+				'}';
+	}
+
+	@Override
+	public int compareTo(Activity activity) {
+		return this.getEndTime() - activity.getEndTime();
+
+
+	}
+}
