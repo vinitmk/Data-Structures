b502154d0321fe2579bd272f7f1fead8f07629d5
vinitmk <vinit.kallampad@gmail.com>
2021-01-03 21:47:44 -0800

    Activity Selection algorithm

---
+++ GreedyAlgorithms/src/com/mkv/algorithms/greedy/activityselection/ActivitySelection.java
@@ -0,0 +1,25 @@
+package com.mkv.algorithms.greedy.activityselection;
+
+import java.util.Collections;
+import java.util.List;
+
+class ActivitySelection {
+
+	void selectActivity(List<Activity> activityList) {
+
+		if(activityList.size() > 0) {
+			Collections.sort(activityList);
+			Activity previousActivity = activityList.get(0);
+
+			System.out.println("\n\nRecommended Schedule:\n" + previousActivity);
+
+			for(int i = 1; i < activityList.size(); i++) {
+				Activity currentActivity = activityList.get(i);
+				if(currentActivity.getStartTime() >= previousActivity.getEndTime()) {
+					System.out.println(currentActivity);
+					previousActivity = currentActivity;
+				}
+			}
+		}
+	}
+}
