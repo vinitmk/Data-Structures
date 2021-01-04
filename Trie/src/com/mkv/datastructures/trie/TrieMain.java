bcd193b56de020714ee51b47e02af91cd0bcd7e4
vinitmk <vinit.kallampad@gmail.com>
2021-01-03 20:02:17 -0800

    Trie Pending

--- Trie/src/com/mkv/datastructures/trie/TrieMain.java
+++ Trie/src/com/mkv/datastructures/trie/TrieMain.java
@@ -1,5 +1,5 @@
 package com.mkv.datastructures.trie;
-
+//todo
 public class TrieMain {
 
 	private TrieNode root;
@@ -7,15 +7,15 @@ public class TrieMain {
 	TrieMain() {
 		root = new TrieNode();
 	}
-
+	//todo
 	public void insert(String word) {
 
 	}
-
+	//todo
 	public boolean search(String word) {
 		return false;
 	}
-
+	//todo
 	public void delete(String word) {
 
 	}
