c54dfdb71d5128366d006334041dc3300172cbf5
vinitmk <vinit.kallampad@gmail.com>
2021-01-05 18:33:50 -0800

    Divide and Conquer: House Thief

---
+++ DivideAndConquer/src/com/mkv/algorithms/divideandconquer/HouseThief.java
@@ -0,0 +1,24 @@
+package com.mkv.algorithms.divideandconquer;
+
+public class HouseThief {
+
+	private int getMaximumValue(int[] houseValues, int currentIndex) {
+		if(currentIndex >= houseValues.length)
+			return 0;
+
+		int currentHouseValue = houseValues[currentIndex] + getMaximumValue(houseValues, currentIndex + 2);
+		int nexHouseValue = getMaximumValue(houseValues, currentIndex+1);
+		return Math.max(currentHouseValue, nexHouseValue);
+	}
+
+	public static void main(String[] args) {
+		HouseThief main = new HouseThief();
+		int[] houses = {6, 7, 1, 30, 8, 2, 4};
+		int totalValue = main.getMaximumValue(houses,0);
+		System.out.println("Total Value "+ totalValue);
+
+		houses = new int[] {20, 5, 1, 13, 6, 11, 40};
+		totalValue = main.getMaximumValue(houses,0);
+		System.out.println("Total Value "+ totalValue);
+	}
+}
