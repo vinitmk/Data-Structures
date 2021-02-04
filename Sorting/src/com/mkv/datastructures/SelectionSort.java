ebb31e4ab6732393884dcc8cfc2aa8e4b33aaf41
vinitmk <vinit.kallampad@gmail.com>
2021-02-04 08:06:01 -0800

    Sorting: Selection sort

---
+++ Sorting/src/com/mkv/datastructures/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.mkv.datastructures;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	private void sort(int[] array) {
+		int temp, minIndex;
+		for(int i = 0; i < array.length; i++) {
+			minIndex = i;
+			for(int j = i+1; j < array.length; j++) {
+				if(array[j] < array[minIndex])
+					minIndex = j;
+			}
+			if(minIndex != i) {
+				temp = array[minIndex];
+				array[minIndex] = array[i];
+				array[i] = temp;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		SelectionSort main = new SelectionSort();
+
+		int[] array = new int[]{100, 67, 89, 10, 54, 23, 11};
+		main.sort(array);
+		System.out.println(Arrays.toString(array));
+	}
+}
